cae9144fa997181ef36f03e12191c8afce4f5678
Jafooly <84318706+Jafooly3004@users.noreply.github.com>
2021-06-02 20:59:41 +0100

    INITIAL UPLOAD
    First TEST upload of 0.4.5 for MH and DM review / testing.

---
+++ JAFINSTALLATION - 0.1 - 020621.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#DEBIAN / UBUNTU
+#sudo apt-get update
+#sudo apt-get install zenity -y
+#sudo apt-get install python -y
+
+
+#REDHAT
+#dnf install zenity
+#dnf install python
+
+#ARCH
+#pacman -S zenity
+#pacman -S python
+
+sudo chmod +x jaf.sh
+sudo cp jaf.sh /usr/local/bin/jaf.sh
